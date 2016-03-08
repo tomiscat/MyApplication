4b6d41d0f379225cefa3f8f95e3808aa48b47905
fov <tomiscat.x@foxmail.com>
2016-03-08 19:23:51 +0800

    add butterknife

--- app/src/main/java/cc/usopp/myapplication/MainActivity.java
+++ app/src/main/java/cc/usopp/myapplication/MainActivity.java
@@ -5,36 +5,60 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import org.robobinding.ViewBinder;
 import org.robobinding.binder.BinderFactory;
 import org.robobinding.binder.BinderFactoryBuilder;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ActivityDemo";
+
+    @Bind(R.id.edit)
+    EditText edit;
+
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        /*
+            ===robobinding绑定===
+            PresentationModel presentationModel = new PresentationModel();
+            ViewBinder viewBinder = createViewBinder();
+            View rootView = viewBinder.inflateAndBind(R.layout.activity_main, presentationModel);
+            setContentView(rootView);
+        */
+    }
 
-        PresentationModel presentationModel = new PresentationModel();
-        ViewBinder viewBinder = createViewBinder();
-        View rootView = viewBinder.inflateAndBind(R.layout.activity_main, presentationModel);
-        setContentView(rootView);
+    @OnClick(R.id.txtView)
+    void onSubmit() {
+        Log.d(TAG, "txtView onSubmit~~~");
+    }
+
+    @OnClick(R.id.fab)
+    void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 
     private ViewBinder createViewBinder() {
@@ -62,5 +86,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "start onStart~~~");
+        Log.d(TAG, "edit.getText() : " + edit.getText());
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "start onResume~~~");
+        Log.d(TAG, "edit.getText() : " + edit.getText());
+//        edit.setText(edit.getText());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "start onPause~~~");
+        Log.d(TAG, "edit.getText() : " + edit.getText());
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
     }
 }
